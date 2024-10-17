1e2f06c060f0a3e534ac2a48eac9526d0782fee4
kymkrt <wh40k@naver.com>
2024-10-17 23:41:44 +0900

    20241017_밤

---
+++ src/main/java/practice/hw2.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hw2")
+public class hw2 extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		System.out.println("hw2");
+		
+		String selec = request.getParameter("selec")==null ? "" : request.getParameter("selec");
+		
+		request.setAttribute("img", selec);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/study/practice/hw2.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+}
